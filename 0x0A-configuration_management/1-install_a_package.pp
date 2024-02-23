08450c7b79e39a28af2d76fec6cb5ede61dbba4d
ade-mic <ademic.aina@gmail.com>
2024-02-23 18:44:26 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }

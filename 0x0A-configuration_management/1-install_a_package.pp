b68c96f964d52f2dc2bfe69162c890ac76a3301f
ade-mic <ademic.aina@gmail.com>
2024-02-23 19:10:16 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -13,7 +13,7 @@ exec { 'install_werkzeug':
 
 # Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
-  command => 'pip3 install',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }

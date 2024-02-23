1628c4103437533de8249508a796647d20ae596c
ade-mic <ademic.aina@gmail.com>
2024-02-23 19:08:06 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,19 @@
 # Using Puppet, install flask from pip3
 # Ensure pip3 is installed
 package { 'python3-pip':
-  ensure => installed,
+  ensure => 'installed',
+}
+
+# Install Werkzeug version 2.1.1 using pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
 
 # Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => 'pip3 install',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }

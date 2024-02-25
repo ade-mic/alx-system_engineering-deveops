76108fd37e57b8c1ba1adfbc63f8e9944db50d72
ade-mic <ademic.aina@gmail.com>
2024-02-25 18:57:20 +0100

    check 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # make changes to SSH  configuration file
 file_line { 'ssh_config_keyfile':
-  path    => '/home/ademich/.ssh/config',
+  path    => '/home/vagrant/.ssh/config',
   line    => "IdentityFile ~/.ssh/school",
   match   => "IdentityFile <existing_keyfile>",
   ensure  => present,
 }
 file_line { 'modify_ssh_config':
-  path    => '/etc/ssh/sshd_config',
+  path    => '/home/vagrant/.ssh/config'
   line    => 'PasswordAuthentication no',
   match   => '^#?PasswordAuthentication',
   ensure  => present,

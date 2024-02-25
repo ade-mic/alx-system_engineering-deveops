00d2d353270ae0b7b573fa3caf65ea57989b1077
ade-mic <ademic.aina@gmail.com>
2024-02-25 19:03:47 +0100

    chV

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,11 @@
 # Puppet manifest to configure SSH client
 
-# Install ssh client package
-package { 'openssh-client':
-  ensure => installed,
-}
-
 # Ensure SSH directory exists
 file { '/home/vagrant/.ssh':
   ensure => directory,
   mode   => '0700',
-  owner  => 'user',
-  group  => 'user',
+  owner  => 'vagrant',
+  group  => 'vagrant',
 }
 
 # Copy private key to SSH directory
@@ -18,15 +13,20 @@ file { '/home/vagrant/.ssh/school':
   ensure => present,
   source => 'puppet:///modules/ssh/school',
   mode   => '0600',
-  owner  => 'user',
-  group  => 'user',
+  owner  => 'vagrant',
+  group  => 'vagrant',
 }
 
-# Configure SSH client to use private key and refuse password authentication
+# Ensure SSH client configuration file exists
 file { '/home/vagrant/.ssh/config':
   ensure  => present,
-  content => "Host server\n  Hostname example.com\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
   mode    => '0600',
-  owner   => 'user',
-  group   => 'user',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  content => "
+    Host example-server
+      HostName example.com
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }

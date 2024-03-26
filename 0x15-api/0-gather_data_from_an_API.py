b7f9a9c872ab6dd84033728a653d51fc3a6f30c7
ade-mic <ademic.aina@gmail.com>
2024-03-26 02:27:26 +0100

    0-gather_data_from_an_API.py_checker

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,8 +4,8 @@ a Python script that, using this REST API, for a given employee ID, returns
 information about his/her TODO list progress.
 """
 
-from sys import argv
 import json
+from sys import argv
 import requests
 
 
@@ -36,7 +36,7 @@ def main():
         employee_name, completed_tasks, total_tasks))
     for task in todo_data:
         if task['completed']:
-            print('\t{}'.format(task['title']))
+            print('\t {}'.format(task['title']))
 
 
 if __name__ == "__main__":

4063290925c461eb97ba830bee340f6a3d28bb03
ade-mic <ademic.aina@gmail.com>
2024-04-09 10:36:22 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,17 +10,13 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
 
-    try:
-        # make request to Reddit API
-        response = requests.get(url, headers=headers)
-        # Check if the subreddit is valid by looking at the status code
-        if response.status_code == 200:
-            # Parse the JSON response and return the subscriber count
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # id the status code is not 200
-            return 0
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    # make request to Reddit API
+    response = requests.get(url, headers=headers)
+    # Check if the subreddit is valid by looking at the status code
+    if response.status_code == 200:
+        # Parse the JSON response and return the subscriber count
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        # id the status code is not 200
         return 0

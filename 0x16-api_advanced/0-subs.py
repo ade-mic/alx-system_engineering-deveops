558c732c54518edf45317028f5911e79334cc928
ade-mic <ademic.aina@gmail.com>
2024-04-09 11:51:03 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-function that queries the Reddit API and returns the number of subscribers
+""" function that queries the Reddit API and returns the number of subscribers
 If an invalid subreddit is given, the function should return 0
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """function that queries Reddit"""
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
 
     # make request to Reddit API
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, timeout=5)
     # Check if the subreddit is valid by looking at the status code
     if response.status_code == 200:
         # Parse the JSON response and return the subscriber count

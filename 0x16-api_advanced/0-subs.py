e84d28e44ae9707fc7eac1e209c15c2c5428a42c
ade-mic <ademic.aina@gmail.com>
2024-04-09 12:00:26 +0100

    0-subx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,15 @@ def number_of_subscribers(subreddit):
     """function that queries Reddit"""
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'subreddit_subscriber_counter/0.1'}
+    headers = {'User-Agent': 'Custom Subscriber'}
 
     # make request to Reddit API
-    response = requests.get(url, headers=headers, timeout=5)
+    response = requests.get(url, headers=headers, timeout=5, allow_redirects=False)
     # Check if the subreddit is valid by looking at the status code
     if response.status_code == 200:
         # Parse the JSON response and return the subscriber count
         data = response.json()
-        return data['data']['subscribers']
+        return(data['data']['subscribers'])
     else:
         # id the status code is not 200
-        return 0
+        return(0)

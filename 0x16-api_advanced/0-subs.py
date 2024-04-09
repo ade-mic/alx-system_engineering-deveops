1897c10326cc22503c788825f5fbdfb645b237d7
ade-mic <ademic.aina@gmail.com>
2024-04-09 16:04:55 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,20 +7,27 @@ for a given subreddit.
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(subreddit, after=None):
     """
-    Function to query the Reddit API and return the number of subscribers
-    for a given subreddit. If an invalid subreddit is given, the function
+    Recursive function to query the Reddit API and return the
+    number of subscribersfor a given subreddit.
+    If an invalid subreddit is given, the function
     returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom'}
-    params = {'count': 1}
+    params = {'after': after}
 
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
 
-    return response.json()['data']['subscribers']
+    data = response.json()['data']
+    subscribers = data.get('subscribers', 0)
+
+    if data['after'] is not None:
+        subscribers += number_of_subscribers(subreddit, data['after'])
+
+    return subscribers
